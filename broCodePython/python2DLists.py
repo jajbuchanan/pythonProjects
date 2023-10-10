b0e68a4e13f84f5ac2a03cac6182fb9e261c83c5
Jonathan Buchanan <axtract@gmail.com>
2023-10-10 23:05:44 +0000

    add 2d lists example

---
+++ broCodePython/python2DLists.py
@@ -0,0 +1,17 @@
+# 2D lists: a list of lists
+
+drinks = ["coffee", "soda", "tea"]
+dinner = ["pizza", "hamburger", "hotdog"]
+dessert = ["cake", "ice cream"]
+
+food = [drinks, dinner, dessert]
+
+print(food)
+
+
+for x in food: 
+    print(x)
+
+print(food[1])
+
+
